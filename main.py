72da07fd9c37a5257264ec7227014023b0a732ac
Prateek Katam <pkatam@northrop.com.au>
2023-06-13 16:00:47 +1000

    init commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from typing import Union
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/weatherdata")
+def weather_data():
+    return {"data": [
+      ["1/1/2017", "The Phone Company", "Communications", "120"],
+      ["1/2/2017", "Northwind Electric Cars", "Transportation", "142.33"],
+      ["1/5/2017", "Best For You Organics Company", "Groceries", "27.9"],
+      ["1/10/2017", "Coho Vineyard", "Restaurant", "33"],
+      ["1/11/2017", "Bellows College", "Education", "350.1"],
+      ["1/15/2017", "Trey Research", "Other", "135"],
+      ["1/15/2017", "Best For You Organics Company", "Groceries", "97.88"],
+    ]}
