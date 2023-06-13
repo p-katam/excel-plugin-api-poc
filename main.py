ba6e9332d932a7df812cb0c3f93ec3e181bbaf68
Prateek Katam <pkatam@northrop.com.au>
2023-06-14 09:29:34 +1000

    headers added to weather data

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 
 app = FastAPI()
 
@@ -13,14 +13,25 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+
 @app.get("/weatherdata")
 def weather_data():
-    return {"data": [
-      ["1/1/2017", "The Phone Company", "Communications", "120"],
-      ["1/2/2017", "Northwind Electric Cars", "Transportation", "142.33"],
-      ["1/5/2017", "Best For You Organics Company", "Groceries", "27.9"],
-      ["1/10/2017", "Coho Vineyard", "Restaurant", "33"],
-      ["1/11/2017", "Bellows College", "Education", "350.1"],
-      ["1/15/2017", "Trey Research", "Other", "135"],
-      ["1/15/2017", "Best For You Organics Company", "Groceries", "97.88"],
-    ]}
+    data = [
+        ["1/1/2017", "The Phone Company", "Communications", "120"],
+        ["1/2/2017", "Northwind Electric Cars", "Transportation", "142.33"],
+        ["1/5/2017", "Best For You Organics Company", "Groceries", "27.9"],
+        ["1/10/2017", "Coho Vineyard", "Restaurant", "33"],
+        ["1/11/2017", "Bellows College", "Education", "350.1"],
+        ["1/15/2017", "Trey Research", "Other", "135"],
+        ["1/15/2017", "Best For You Organics Company", "Groceries", "97.88"],
+    ]
+
+    response = {"data": data}
+
+    # Add the headers to the response
+    headers = {
+        "Access-Control-Allow-Origin": "*",  # Allow requests from any origin
+        "Access-Control-Allow-Headers": "Content-Type",  # Specify allowed headers
+    }
+
+    return Response(content=response, headers=headers)
