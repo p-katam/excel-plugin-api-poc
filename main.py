94e76c440b42b83685a0fb26af27e2ded2d3e79d
Prateek Katam <pkatam@northrop.com.au>
2023-06-14 10:28:40 +1000

    cors added

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from typing import Union
 from fastapi import FastAPI, Response
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+app = FastAPI(title="Experimental API for fetching BOM data from Postgres")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
